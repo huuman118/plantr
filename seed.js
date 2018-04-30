fa9a0816096c3e7b16929d56da1cc8afcd30a1dc
Huu Nguyen <huu.nguyen@gmail.com>
2018-04-30 18:02:17 -0400

    finished seed

--- seed.js
+++ seed.js
@@ -1,8 +1,26 @@
-const db = require("./models");
+const { db } = require("./models");
+const { Vegetable } = require("./models");
 
 const dbSync = db
   .sync({ force: true })
   .then(() => console.log("works!"))
-  .catch((error) => console.log(`doesn't work!`+ error));
+  .then(() => {
+    return Vegetable.create({
+      name: "Tomato",
+      color: "red",
+      planted_on: Date.now()
+    });
+  })
+  .catch(error => console.log(`doesn't work!` + error))
+  .finally(() => db.close());
 
+// const firstVeg = Vegetable.create({
+//   name: "Tomato",
+//   color: "red",
+//   planted_on: Date.now()
+// });
+
+// firstVeg.save();
 // db.close();
+
+// console.log(Date.now());
